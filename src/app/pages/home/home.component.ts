6b0aac3745ee0eb6070d8ef69eb4d5b83c9122a3
BrendoSilva <brendoricardo00@gmail.com>
2024-12-27 21:07:06 -0300

    ajustes de pastas e rotas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,9 @@
 import { Component, input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { TaskListComponentComponent } from './components/task-list-component/task-list-component.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'; // Importa o módulo FontAwesome
 import { faBook } from '@fortawesome/free-solid-svg-icons';  // Importa o ícone de livro
 import { faSearch } from '@fortawesome/free-solid-svg-icons';  // Importa o ícone de livro
-import { AnalyticsComponent } from './components/analytics/analytics.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { SettingsComponent } from './components/settings/settings.component';
 import { faBarsProgress } from '@fortawesome/free-solid-svg-icons';  // Importa o ícone de task
 import { faChartLine } from '@fortawesome/free-solid-svg-icons';  // Importa o ícone de Dashboard
 import { faTableColumns } from '@fortawesome/free-solid-svg-icons';  // Importa o ícone de banco de dados
@@ -15,13 +11,12 @@ import { faDatabase } from '@fortawesome/free-solid-svg-icons';  // Importa o í
 import { faGear } from '@fortawesome/free-solid-svg-icons';  // Importa o ícone de config
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, TaskListComponentComponent, FontAwesomeModule, AnalyticsComponent, DashboardComponent, SettingsComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: 'app-home',
+  imports: [FontAwesomeModule, CommonModule, RouterOutlet],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
 })
-export class AppComponent {
-  title:string = 'toDoList';
+export class HomeComponent {
   blocoAdicionarTarefa: boolean = false
   faBook = faBook
   faDatabase=faDatabase
@@ -31,13 +26,5 @@ export class AppComponent {
   faChartLine=faChartLine
   faSearch = faSearch
   page:string = ""
-
-
-  constructor(){
-   
-  }
-  alterarPage(value: any) {
-    this.page = value; // Atualiza o valor recebido
-  }
-
+  
 }
