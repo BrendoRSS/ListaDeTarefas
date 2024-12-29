7e25bb64e9640884f7b9b7d40fe4e13c9c49763b
BrendoSilva <brendoricardo00@gmail.com>
2024-12-29 14:12:46 -0300

    funcionalidade de adição de cards nas etapas

--- src/app/components/card-tarefa/card-tarefa.component.ts
+++ src/app/components/card-tarefa/card-tarefa.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'; // Importa o módulo FontAwesome
 import { faPen } from '@fortawesome/free-solid-svg-icons'; // Importa o ícone
 import { faTrash } from '@fortawesome/free-solid-svg-icons';  // Importa o ícone de lixeira
@@ -11,4 +11,7 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';  // Importa o ícon
 export class CardTarefaComponent {
 faPen = faPen;  // Ícone de lápis
 faTrash = faTrash // Ícone de lixeira
+
+@Input() title: string = '';
+@Input() description: string = '';
 }
--- src/app/components/task-list-component/task-list-component.component.ts
+++ src/app/components/task-list-component/task-list-component.component.ts
@@ -1,27 +1,45 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'; // Importa o módulo FontAwesome
-import { faPen } from '@fortawesome/free-solid-svg-icons'; // Importa o ícone
-
-import { faSearch } from '@fortawesome/free-solid-svg-icons';  // Importa o ícone de pesquisa
-import { faPlus } from '@fortawesome/free-solid-svg-icons';  // Importa o ícone de add
-import { faEllipsis } from '@fortawesome/free-solid-svg-icons';  // Importa o ícone de add
-import { faFilter } from '@fortawesome/free-solid-svg-icons';  // Importa o ícone de filtro
-import { faBell } from '@fortawesome/free-solid-svg-icons';  // Importa o ícone de sino
-import { faCircleQuestion } from '@fortawesome/free-solid-svg-icons';  // Importa o ícone de interrogação
+import { faPen, faSearch, faPlus, faEllipsis, faFilter, faBell, faCircleQuestion } from '@fortawesome/free-solid-svg-icons'; // Importa ícones
 import { CardTarefaComponent } from '../card-tarefa/card-tarefa.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-list-component',
-  imports: [FontAwesomeModule, CardTarefaComponent ],
+  imports: [FontAwesomeModule, CardTarefaComponent, CommonModule ],
   templateUrl: './task-list-component.component.html',
   styleUrl: './task-list-component.component.scss'
 })
 export class TaskListComponentComponent {
-  faPen=faPen
+  faPen = faPen
   faSearch = faSearch // Ícone de pesquisa
   faPlus = faPlus // Ícone de add
   faEllipsis = faEllipsis // Ícone de 3 pontos
   faFilter = faFilter // Ícone de filtro
   faBell = faBell // Ícone de sino
   faCircleQuestion = faCircleQuestion //ícone de interrogação
+  popUp:boolean = true // Variável para abrir popUp
+
+  tasks: any = {
+    toDo: [] as { title: string; description: string }[],
+    inProgress: [] as { title: string; description: string }[],
+    done: [] as { title: string; description: string }[],
+  };
+
+  addCard(stage:string){
+    const title = prompt('Digite o título da nova tarefa:');
+    if (!title || !title.trim()) return; // Validação de título
+
+    const description = prompt('Digite a descrição da nova tarefa:');
+
+    if (!description || !description.trim()) return; // Validação de descrição
+
+    this.tasks[stage].push({ 
+      title: title.trim(), 
+      description: description.trim() });
+  }
+
+  togglePopUp() {
+    this.popUp = !this.popUp;
+}
 }
